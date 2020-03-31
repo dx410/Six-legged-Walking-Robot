df67c2fe4a109b897b6e4cc320ec87bf53913a9a
Xiao Deng <62615723+dx410@users.noreply.github.com>
2020-03-31 16:38:25 +0100

    Create servo test.cpp

---
+++ code/Unit Testing/servo test.cpp
@@ -0,0 +1,97 @@
+#include <iostream>
+#include <wiringPi.h>
+#include <softPwm.h>
+#include <stdlib.h>
+
+using namespace std;
+#define pwm_right 1
+#define pwm_left 4
+#define pwm_tilt 6
+
+void forward()
+{
+	 softPwmCreate(pwm_tilt,8,100);
+	 delay(800);
+	 softPwmCreate(pwm_left,20,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,20);
+	 delay(800);
+	 softPwmCreate(pwm_right,8,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,15);
+	 delay(800);
+	 softPwmWrite(pwm_left,15);
+	 delay(800);
+	 softPwmWrite(pwm_right,15);
+	 delay(800);	 
+	 exit(0);
+}	
+
+void backward()
+{
+	 softPwmCreate(pwm_tilt,8,100);
+	 delay(800);
+	 softPwmCreate(pwm_left,8,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,20);
+	 delay(800);
+	 softPwmCreate(pwm_right,20,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,15);
+	 delay(800);
+	 softPwmWrite(pwm_left,15);
+	 delay(800);
+	 softPwmWrite(pwm_right,15);
+	 delay(800);	 
+	 exit(0);
+}
+
+void left()
+{ 
+	 softPwmCreate(pwm_tilt,8,100);
+	 delay(800);
+	 softPwmCreate(pwm_left,20,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,20);
+	 delay(800);
+	 softPwmCreate(pwm_right,20,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,15);
+	 delay(800);
+	 softPwmWrite(pwm_left,15);
+	 delay(800);
+	 softPwmWrite(pwm_right,15);
+	 delay(800);	 
+	 exit(0);
+
+}
+
+void right()
+{
+	softPwmCreate(pwm_tilt,8,100);
+	 delay(800);
+	 softPwmCreate(pwm_left,8,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,20);
+	 delay(800);
+	 softPwmCreate(pwm_right,8,100);
+	 delay(800);
+	 softPwmWrite(pwm_tilt,15);
+	 delay(800);
+	 softPwmWrite(pwm_left,15);
+	 delay(800);
+	 softPwmWrite(pwm_right,15);
+	 delay(800);	 
+	 exit(0);
+
+	
+}
+
+int main()
+{
+	 wiringPiSetup();
+	 forward();
+   backward();
+   left();
+   right();
+}
