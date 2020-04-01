ab28ff53678a5822b369bddd34d522cf20579d38
Xiao Deng <62615723+dx410@users.noreply.github.com>
2020-04-01 15:30:41 +0100

    Update widget.h

--- code/Unit Testing/Threads/1-thread/widget.h
+++ code/Unit Testing/Threads/1-thread/widget.h
@@ -24,9 +24,9 @@ public:
     ~Widget();
 
 private slots:
-    void creatConnection(); //创建一个新连接
-    void useConnection(); //使用当前连接
-    void receiveMessage(); //接收消息
+    void creatConnection(); 
+    void useConnection(); 
+    void receiveMessage(); 
 
     void openThreadBtnSlot();
     void closeThreadBtnSlot();
