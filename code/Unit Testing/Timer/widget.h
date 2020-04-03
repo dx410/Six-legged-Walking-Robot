ded66ea9581fddfc368b390bb80322c3a6fa6dd6
Xiao Deng <62615723+dx410@users.noreply.github.com>
2020-04-03 22:21:03 +0100

    Update widget.h

--- code/Unit Testing/Timer/widget.h
+++ code/Unit Testing/Timer/widget.h
@@ -28,14 +28,5 @@ private:
     Ui::Widget *ui;
 };
 
-/*
-class Sleeper : public QThread
-{
-public:
-    static void usleep(unsigned long usecs){QThread::usleep(usecs);}
-    static void msleep(unsigned long msecs){QThread::msleep(msecs);}
-    static void sleep(unsigned long secs){QThread::sleep(secs);}
-};
-*/
 
 #endif // WIDGET_H
